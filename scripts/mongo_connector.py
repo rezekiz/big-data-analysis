b2ccc34da2c5f11bb7e613bb2d441589674e3dec
Rui S <contact@ruisousa.me>
2024-04-05 19:53:18 +0100

    reorganização

---
+++ scripts/mongo_connector.py
@@ -0,0 +1,39 @@
+import sys
+
+# Setting path
+sys.path.append('../datasets')
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pandas as pd
+
+uri = "mongodb+srv://bigdata:bioinfo24@bigdata.l3at7tn.mongodb.net/?retryWrites=true&w=majority&appName=BigData"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Aceder à DB 
+db = client.get_database("BigData")
+
+# Aceder à colecção
+collection = db.get_collection("ObesPovMen")
+
+# Read CSV file using pandas
+csv_file = "../datasets/processed/dataset_final.csv"
+data = pd.read_csv(csv_file)
+
+# Convert DataFrame to dictionary
+data_dict = data.to_dict(orient='records')
+
+# Insert data into MongoDB collection
+collection.insert_many(data_dict)
+
+# Close connection
+client.close()
